4b6e179d912e8d17dbc079a6788d44cad5cc421a
khaoula <khaoulabenrhebra@gmail.com>
2024-09-03 15:40:49 +0200

    affi et saisie du tab

--- challange3.c
+++ challange3.c
@@ -2,5 +2,13 @@
 #include<stdlib.h>
 int main ()
 {
-    
+    int i,N,T[N];
+    printf("veuillez entrer la taille de votre tableau");
+    scanf("%d",N);
+    printf("veuilez saisir les elements de votre tableau");
+    for(i=0;i<N;i++)
+    {
+        printf("T[%d]= ",i);
+        scanf("%d",&T[N]);
+    }
 }

c98b732ad8653a43d3b6a71402d054566295599a
khaoula <khaoulabenrhebra@gmail.com>
2024-09-03 15:38:17 +0200

    somme des elements du tab

--- challange2.c
+++ challange2.c
@@ -2,7 +2,7 @@
 #include<stdlib.h>
 int main ()
 {
-int i,N,T[N];
+int i,N,T[N],S;
 printf("veuillez entrer la taillle de votre tableau : ");
 scanf("%d",&N);
 printf("veuillez entrer les elements de votre tableau: \n");
@@ -11,5 +11,9 @@ for(i=0;i<N;i++)
     printf("T[%d]= ",i) ;
     scanf("%d",&T[N]);
 }
+S=0;
+for(i=0;i<N;i++)
+    S=S+T[N];
+printf("la somme des elements du tableau est : %d",S);
 return 0;
 }

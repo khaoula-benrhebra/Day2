4197537ec682a387c2da3c89c5f0da2274ae7a5e
khaoula <khaoulabenrhebra@gmail.com>
2024-09-03 15:19:32 +0200

    affichage du tableau

---
+++ challage1.c
@@ -0,0 +1,9 @@
+#include<stdlib.h>
+#include<stdio.h>
+int main ()
+{
+int i,T[4]={1,2,3,4,5};
+for(i=0;i<4;i++)
+printf("T[%d]=%d \n",i,T[i]);
+return 0;
+}
---
+++ challange2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main ()
+{
+int i,N,T[N];
+printf("veuillez entrer la taillle de votre tableau : ");
+scanf("%d",&N);
+printf("veuillez entrer les elements de votre tableau: \n");
+for(i=0;i<N;i++)
+{
+    printf("T[%d]= ",i);
+    scanf("%d",&T[N]);
+}
+return 0;
+}

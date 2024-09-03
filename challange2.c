e01a4a168dbc79a5a1afda9d2dd0caca32d21cad
khaoula <khaoulabenrhebra@gmail.com>
2024-09-03 18:04:33 +0200

    somme des elements

--- challange2.c
+++ challange2.c
@@ -2,9 +2,10 @@
 #include<stdlib.h>
 int main ()
 {
-int i,N,T[N],S;
+int i,N,S;
 printf("veuillez entrer la taillle de votre tableau : ");
 scanf("%d",&N);
+int T[N];
 printf("veuillez entrer les elements de votre tableau: \n");
 for(i=0;i<N;i++)
 {
